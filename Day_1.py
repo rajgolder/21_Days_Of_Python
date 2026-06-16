d34528577b1dc73b4617a5e6024725a814a53e3e
Raj <raj.golder24@gmail.com>
2026-06-16 15:05:31 +0530

    Day 1

---
+++ Day_1.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+print(2 + 3) #addition
+print(3 - 1) #subtraction
+print(2 * 3) #multiplication
+print(3 / 2) # ivision
+print(3 ** 2) #exponential(to the power of)
+print(3 % 2) #modulus(reminder)
+print(3 // 2) #floor division operator(Divides the numbers and removes the decimal part (rounds down to the nearest whole number))
+
+print(type(10)) #int
+print(type(3.14)) #float
+print(type(1 + 3j)) #complex
+print(type("Python")) #string
+print(type({'name': 'Python'})) #dictionary
+print(type({9.8, 3.14, 2.7})) #tuple
